1f4f440b4207a876d774041227da6c1ec671f0c2
Alex Olieman <alex@olieman.net>
2018-02-27 21:34:41 +0100

    add url/querystring to alerts

--- ocd_frontend/rest/views.py
+++ ocd_frontend/rest/views.py
@@ -448,6 +448,7 @@ def subscribe_search():
             'token': token,
             'activated': False,
             'query': es_query['query'],
+            'querystring': data['url'],
         },
     )
 
